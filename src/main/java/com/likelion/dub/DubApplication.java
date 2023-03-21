6b04007e825ff70d12fb2caa0f64b9f69baa3f8c
suhoon <jobak4200nim@nate.com>
2023-03-21 19:38:45 +0900

    first commit

---
+++ src/main/java/com/likelion/dub/DubApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.dub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/dub/DubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.dub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
