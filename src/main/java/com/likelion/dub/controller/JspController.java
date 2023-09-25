ff8647d615cd6b3ffa001eb3e6d844c5f3d35e76
suhoon <jobak4200nim@nate.com>
2023-09-25 18:12:34 +0900

    feat: thymeleaf 방식 추가

--- src/main/java/com/likelion/dub/controller/JspController.java
+++ src/main/java/com/likelion/dub/controller/JspController.java
@@ -4,19 +4,17 @@ package com.likelion.dub.controller;
 import com.likelion.dub.domain.dto.MemberJoinRequest;
 import com.likelion.dub.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*") //Cors 제거
 public class JspController {
 
     private final MemberService memberService;
@@ -26,39 +24,18 @@ public class JspController {
 
     @GetMapping("/login")
     public String loginView() {
-        String result = "loginVIew";
-
         return "loginView";
     }
 
     @GetMapping("/redirect")
-    public @ResponseBody String kakaoCallback(String code) {
-        String url = "http://localhost:8080/app/member/loginKakao";
-
-        // 요청 바디 생성
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("authorizationCode", code);
-
-        // HTTP 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        System.out.println(code);
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        // JSONObject를 JSON 문자열로 변환하여 출력
-        System.out.println(requestBody.toString());
-        // HttpEntity 생성
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
-
-
-        System.out.println("HttpEntity: " + requestEntity);
-        // restTemplate을 사용하여 JSON 요청 보내기
-        String response = restTemplate.postForObject(url, requestEntity, String.class);
-        return "Bearer " + response;
+    public String kakaoCallback(@RequestParam("code") String code, RedirectAttributes redirectAttributes) {
+        // code 값을 main 페이지로 전달
+        redirectAttributes.addFlashAttribute("authorizationCode", code);
+        return "redirect:/mainView"; // mainView 페이지로 리다이렉트
     }
 
-    @GetMapping("/main")
-    public String mainView(Model model) {
-        String result = "mainView";
-        model.addAttribute("result", result);
+    @GetMapping("/mainView")
+    public String mainView() {
         return "mainView";
     }
 
